f0dfd4b83b728288b67a3989c188affcb4ae0e3d
J-millar99 <wlwogus33@gmail.com>
2025-05-23 14:14:07 +0900

    MemberRepository add

--- src/main/java/com/example/library/repository/MemberRepository.java
+++ src/main/java/com/example/library/repository/MemberRepository.java
@@ -1,4 +1,20 @@
 package com.example.library.repository;
 
-public interface MemberRepository {
+import com.example.library.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 이메일을 통해 해당하는 회원을 검색
+    Optional<Member> findByEmail(String email);
+
+    // 전화번호를 통해 해당하는 회원을 검색
+    Optional<Member> findByPhoneNumber(String phoneNumber);
+
+    // 이름(부분 문자열)을 통해 해당하는 회원을 검색
+    List<Member> findByNameContaining(String name);
 }
