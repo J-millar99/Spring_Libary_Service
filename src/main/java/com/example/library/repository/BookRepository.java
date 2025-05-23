e281f92c09735e4dcdacebdcb65ddd0ad21d09cc
J-millar99 <wlwogus33@gmail.com>
2025-05-23 14:14:28 +0900

    BookRepository update: method

--- src/main/java/com/example/library/repository/BookRepository.java
+++ src/main/java/com/example/library/repository/BookRepository.java
@@ -6,18 +6,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     // 제목(부분 문자열)을 통해 해당하는 모든 도서를 검색
-    public List<Book> findByTitleContaining(String title);
+    List<Book> findByTitleContaining(String title);
 
     // 저자(부분 문자열)를 통해 해당하는 모든 도서를 검색
-    public List<Book> findByAuthorContaining(String author);
+    List<Book> findByAuthorContaining(String author);
 
     // 도서 상태를 통해 해당하는 모든 도서를 검색
-    public List<Book> findByStatus(BookStatus status);
+    List<Book> findByStatus(BookStatus status);
 
     // 저자와 제목을 통해 일치하는 도서를 검색
-    public Book findByAuthorAndTitle(String author, String title);
+    Optional<Book> findByAuthorAndTitle(String author, String title);
 }
